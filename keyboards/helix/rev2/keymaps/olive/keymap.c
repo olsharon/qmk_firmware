82a1492c03515749e2f3a28c2f9c5e1c0b360bcb
olsharon <olivia.sharon6@gmail.com>
2020-10-15 23:41:13 -0400

    added cool ctrl

--- keyboards/helix/rev2/keymaps/olive/keymap.c
+++ keyboards/helix/rev2/keymaps/olive/keymap.c
@@ -52,6 +52,9 @@ enum macro_keycodes {
 //Macros
 #define M_SAMPLE M(KC_SAMPLEMACRO)
 
+//Custom Keys
+#define CTL_PLY MT(MOD_LCTL, KC_MPLY)
+
 #if MATRIX_ROWS == 10 // HELIX_ROWS == 5
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -73,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      KC_LCTL, KC_LGUI, KC_LALT, KC_WSCH, ADJUST,  KC_SPC, LOWER,   RAISE,   KC_SPC,  KC_MINS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      CTL_PLY, KC_LGUI, KC_LALT, KC_PLUS, ADJUST,  KC_SPC, LOWER,   RAISE,   KC_SPC,  KC_MINS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
     ),
 
   /* Colemak
